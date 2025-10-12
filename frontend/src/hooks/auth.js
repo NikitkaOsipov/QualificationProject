d43eec5547dfbfb9223fab22ca53ee9c8cad272c
Nikita <no00499@rvt.lv>
2025-10-12 13:11:43 +0300

    Scuffolded frontend template

---
+++ frontend/src/hooks/auth.js
@@ -0,0 +1,132 @@
+import useSWR from 'swr'
+import axios from '@/lib/axios'
+import { useEffect } from 'react'
+import { useParams, useRouter } from 'next/navigation'
+
+export const useAuth = ({ middleware, redirectIfAuthenticated } = {}) => {
+    const router = useRouter()
+    const params = useParams()
+
+    const { data: user, error, mutate } = useSWR('/api/user', () =>
+        axios
+            .get('/api/user')
+            .then(res => res.data)
+            .catch(error => {
+                if (error.response.status !== 409) throw error
+
+                router.push('/verify-email')
+            }),
+    )
+
+    const csrf = () => axios.get('/sanctum/csrf-cookie')
+
+    const register = async ({ setErrors, ...props }) => {
+        await csrf()
+
+        setErrors([])
+
+        axios
+            .post('/register', props)
+            .then(() => mutate())
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const login = async ({ setErrors, setStatus, ...props }) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/login', props)
+            .then(() => mutate())
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const forgotPassword = async ({ setErrors, setStatus, email }) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/forgot-password', { email })
+            .then(response => setStatus(response.data.status))
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const resetPassword = async ({ setErrors, setStatus, ...props }) => {
+        await csrf()
+
+        setErrors([])
+        setStatus(null)
+
+        axios
+            .post('/reset-password', { token: params.token, ...props })
+            .then(response =>
+                router.push('/login?reset=' + btoa(response.data.status)),
+            )
+            .catch(error => {
+                if (error.response.status !== 422) throw error
+
+                setErrors(error.response.data.errors)
+            })
+    }
+
+    const resendEmailVerification = ({ setStatus }) => {
+        axios
+            .post('/email/verification-notification')
+            .then(response => setStatus(response.data.status))
+    }
+
+    const exceptEmailVerification = () => {
+        axios.post('http://localhost:8000/verify-email/2/95fc8a773d2a77f617254c61393c09562f822809?expires=1760266069&amp;signature=0fa8b5550c8ad8c4992fe9d614a5729bfc5f80b61df401f4353d46c206ff4106')
+            .then(response => console.log(response))
+    }
+
+    const logout = async () => {
+        if (!error) {
+            await axios.post('/logout').then(() => mutate())
+        }
+
+        window.location.pathname = '/login'
+    }
+
+    useEffect(() => {
+        if (middleware === 'guest' && redirectIfAuthenticated && user)
+            router.push(redirectIfAuthenticated)
+
+        if (middleware === 'auth' && (user && !user.email_verified_at))
+            router.push('/verify-email')
+        
+        if (
+            window.location.pathname === '/verify-email' &&
+            user?.email_verified_at
+        )
+            router.push(redirectIfAuthenticated)
+        if (middleware === 'auth' && error) logout()
+    }, [user, error])
+
+    return {
+        user,
+        register,
+        login,
+        forgotPassword,
+        resetPassword,
+        resendEmailVerification,
+        logout,
+        exceptEmailVerification
+    }
+}
