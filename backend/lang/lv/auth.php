d854a608ad5e4f39269c981c6928e8b2d9dbe3f4
Nikita <no00499@rvt.lv>
2026-05-13 09:39:00 +0300

    Added translation of laravel default validation lines (forgot to add auth to commit)

---
+++ backend/lang/lv/auth.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'failed' => 'Autorizācijas dati neatbilst mūsu ierakstiem.',
+    'password' => 'Norādītā parole nav pareiza.',
+    'throttle' => 'Pārāk daudz pieteikšanās mēģinājumu. Lūdzu, mēģiniet vēlreiz pēc :seconds sekundēm.',
+];
+
