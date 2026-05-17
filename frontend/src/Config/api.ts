0482a8fac342891ac1e233e7fff5bf85a66c6a05
Nikita <no00499@rvt.lv>
2026-05-17 21:47:53 +0300

    Fixed some issues in nginx.conf and dockerfile backend required extensions

--- frontend/src/Config/api.ts
+++ frontend/src/Config/api.ts
@@ -1,11 +1,11 @@
 import { Capacitor } from '@capacitor/core'
 
 const isAndroid = Capacitor.getPlatform() === 'android';
-const androidBaseUrl =
-    process.env.NEXT_PUBLIC_ANDROID_BACKEND_URL
-    ?? 'http://10.0.2.2:8000';
-
 const webBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL ?? 'http://localhost:8000';
+const isProduction = process.env.NODE_ENV === 'production';
+const androidBaseUrl = isProduction
+    ? webBaseUrl
+    : (process.env.NEXT_PUBLIC_ANDROID_BACKEND_URL ?? 'http://10.0.2.2:8000');
 
 export const API_BASE_URL = isAndroid
     ? androidBaseUrl
