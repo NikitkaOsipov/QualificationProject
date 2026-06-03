6e081ae4656cd864cd9cf48f5915b5ba2e8dd63e
Nikita <no00499@rvt.lv>
2026-06-03 19:25:36 +0300

    Added my made marker to the map

--- frontend/src/components/Map/MarkerMap.tsx
+++ frontend/src/components/Map/MarkerMap.tsx
@@ -1,14 +1,23 @@
-'use client'
+'use client';
 
-import Map from '@/components/Map/Map'
-import { Marker, MapContainerProps } from 'react-leaflet'
-import type { EventType } from '@/utils/Types'
+import Map from '@/components/Map/Map';
+import { Marker, MapContainerProps } from 'react-leaflet';
+import type { EventType } from '@/utils/Types';
+import L from 'leaflet';
+import { API_BASE_URL } from '@/Config/api';
 
 export type MarkerMapProps = MapContainerProps & {
     markers: EventType[];
     onMarkerClick: (event: EventType) => void;
 
 }
+
+var logoIcon = L.icon({
+    iconUrl: 'Logo.png',
+
+    iconSize:     [45, 45], // size of the icon
+    iconAnchor:   [22.5, 42], // point of the icon which will correspond to marker's location
+});
 
 const MapPage = ({markers, onMarkerClick, ...props}: MarkerMapProps) => {
     const toCoordinate = (value: unknown): number => {
@@ -25,7 +34,9 @@ const MapPage = ({markers, onMarkerClick, ...props}: MarkerMapProps) => {
                     <Marker
                         key={index}
                         position={[toCoordinate(m.address.lat), toCoordinate(m.address.lng)]}
-                        eventHandlers={{ click: () => onMarkerClick(m) }}>
+                        eventHandlers={{ click: () => onMarkerClick(m) }}
+                        icon={logoIcon}
+                    >
                     </Marker>) : <></>
                 }
 
